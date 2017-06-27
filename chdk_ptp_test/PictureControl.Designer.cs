5a1eac6b0b4796d5a90fb4bb25c6719225f869aa
Dmitry Shechtman <damnian@gmail.com>
2017-06-27 20:30:45 +0300

    Move picture buttons Fix tab order

--- chdk_ptp_test/PictureControl.Designer.cs
+++ chdk_ptp_test/PictureControl.Designer.cs
@@ -37,10 +37,10 @@
             // overlaybutton
             // 
             this.overlaybutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.overlaybutton.Location = new System.Drawing.Point(381, 0);
+            this.overlaybutton.Location = new System.Drawing.Point(380, 29);
             this.overlaybutton.Name = "overlaybutton";
             this.overlaybutton.Size = new System.Drawing.Size(75, 23);
-            this.overlaybutton.TabIndex = 22;
+            this.overlaybutton.TabIndex = 2;
             this.overlaybutton.Text = "Get Overlay";
             this.overlaybutton.UseVisualStyleBackColor = true;
             this.overlaybutton.Click += new System.EventHandler(this.overlaybutton_Click);
@@ -53,17 +53,17 @@
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(360, 240);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
             // 
             // getimagebutton
             // 
             this.getimagebutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.getimagebutton.Location = new System.Drawing.Point(300, 0);
+            this.getimagebutton.Location = new System.Drawing.Point(380, 0);
             this.getimagebutton.Name = "getimagebutton";
             this.getimagebutton.Size = new System.Drawing.Size(75, 23);
-            this.getimagebutton.TabIndex = 20;
+            this.getimagebutton.TabIndex = 1;
             this.getimagebutton.Text = "Get Image";
             this.getimagebutton.UseVisualStyleBackColor = true;
             this.getimagebutton.Click += new System.EventHandler(this.getimagebutton_Click);
